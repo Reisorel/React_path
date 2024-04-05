35908e971e0b688f6e8bef45e187b3f47280ce4b
Francois Lerosier <francois.lerosier@gmail.com>
2024-04-05 17:11:18 +0200

    JSX and container

--- 3.Les-bases-de-React/src/App.jsx
+++ 3.Les-bases-de-React/src/App.jsx
@@ -1,8 +1,11 @@
+import Container from "./lessons/1.Le-JSX/Container"
+
 function App() {
 
   return (
     <div className="main-content">
- 
+      <h1>App composant racine</h1>
+      <Container/>
     </div>
   )
 }
---
+++ 3.Les-bases-de-React/src/lessons/1.Le-JSX/Container.jsx
@@ -0,0 +1,10 @@
+function Container() {
+  return (
+    <div>
+      <p>Hello word from container</p>
+    </div>
+  )
+}
+export default Container
+
+// la div retourne ici une seule éxecution de la fonction JSX
