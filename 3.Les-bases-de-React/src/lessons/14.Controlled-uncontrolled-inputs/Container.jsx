7d91b60616294fb08f6391843207afd656e94afb
Francois Lerosier <francois.lerosier@gmail.com>
2024-07-11 18:32:14 +0200

    uncontrolled inputs

--- 3.Les-bases-de-React/src/lessons/14.Controlled-uncontrolled-inputs/Container.jsx
+++ 3.Les-bases-de-React/src/lessons/14.Controlled-uncontrolled-inputs/Container.jsx
@@ -1,27 +1,42 @@
-import {useState, useRef} from "react"
+import { useState, useRef } from "react"
 import "./Container.css"
 
 
 export default function Container() {
- const [state, setState] = useState("")
+  const [state, setState] = useState("")
 
- function handleInput(e) {
-  setState(e.target.value)
- }
+  function handleInput(e) {
+    setState(e.target.value)
+  }
+
+  const emailRef = useRef()
+
+  function handleSubmit(e){
+    e.preventDefault()
+    console.log(emailRef.current.value);
+  }
+
 
   return (
     <div>
       <h1>Controlled vs Uncontrolled inputs </h1>
-        <form>
-          <label htmlFor="pseudo">Votre nom</label>
-          <input
-            onChange={handleInput}
-            value={state}
-            type="text"
-            id="pseudo" />
-            {!state && <p>Veuillez remplir cet input</p>}
-          <button>Envoyer</button>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="pseudo">Votre nom</label>
+        <input
+          onChange={handleInput}
+          value={state}
+          type="text"
+          id="pseudo" />
+        {/* {!state && <p>Veuillez remplir cet input</p>} */}
+        <label htmlFor="email">Votre email</label>
+        <input
+          ref={emailRef}
+          type="email"
+          id="email"
+        />
+
+        <button>Envoyer</button>
+      </form>
 
       <p>Votre pseudo: {state}</p>
     </div>
