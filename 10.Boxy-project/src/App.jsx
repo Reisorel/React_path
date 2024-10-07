78ec9ebdde71519573f949af97976185bf51ee67
Francois Lerosier <francois.lerosier@gmail.com>
2024-10-07 17:25:32 +0200

    basis+redux

--- 10.Boxy-project/src/App.jsx
+++ 10.Boxy-project/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import LeftContainer from "./layouts/LeftContainer"
 
 function App() {
   return (
@@ -7,9 +8,9 @@ function App() {
       <Header />
       <main
         className="flex flex-wrap justify-center items-center
-      flex-grow bg-grey-100 px-10 md:flex-nowrap"
+      flex-grow bg-gray-100 px-10 md:flex-nowrap"
       >
-        {/* <Leftside/> */}
+        <LeftContainer/>
         {/* <Visualization/> */}
       </main>
       <Footer />
---
+++ 10.Boxy-project/src/layouts/shadowList/shadowList.jsx
@@ -0,0 +1,7 @@
+export default function shadowList() {
+  return (
+    <div>
+      <h1>Shadowlist Tik</h1>
+    </div>
+  )
+}
