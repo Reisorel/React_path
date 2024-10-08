5770fbe8f2658219496a90c7b4251b028ad49a8e
Francois Lerosier <francois.lerosier@gmail.com>
2024-10-08 12:08:02 +0200

    cleaning repo

--- 0.sandbox/sandboxreact/src/SayHello.jsx
+++
@@ -1,9 +0,0 @@
-export default function SayHello() {
-  return (
-    <div style={{ border: "1px solid red"}}>
-      <p>Je dis bonjour component</p>
-      <h1>Hola</h1>
-      <p>Benvenidos à todos</p>
-    </div>
-  );
-}
